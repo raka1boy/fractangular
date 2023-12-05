926ebfda64f6efaeca8b78d49c324b5d2109ad25
Chernyshov Artem <artemka2015kek@gmail.com>
2023-12-05 16:04:04 +0300

    yaay it works my gs

--- src/fractal_stuff.zig
+++ src/fractal_stuff.zig
@@ -7,23 +7,22 @@ pub const RGBA = struct{
     blue: u8,
     alpha: u8
 };
-const window_size_x = 1920;
-const window_size_y = 1080;
+
 pub var event:SDL.SDL_Event = undefined;
 pub var window: ?*SDL.SDL_Window = undefined;
 pub var renderer: ?*SDL.SDL_Renderer = undefined;
 pub var mousePos:SDL.SDL_Point = 0;
 pub var realMousePos:SDL.SDL_Point = 0;
 
-pub fn initFractalWindow(comptime getColors:fn(x:i64,y:i64) RGBA) !void {
+pub fn initFractalWindow(comptime getColors:fn(x:i64,y:i64) RGBA,window_size_x:i64,window_size_y:i64) !void {
     if (SDL.SDL_Init(SDL.SDL_INIT_VIDEO) < 0) @panic("SDL Not initialized!\n");
     _ = SDL.SDL_PollEvent(&event);
-    _ = SDL.SDL_CreateWindowAndRenderer(window_size_x, window_size_y, 0, &window, &renderer);
+    _ = SDL.SDL_CreateWindowAndRenderer(@intCast(window_size_x), @intCast(window_size_y), 0, &window, &renderer);
     //event.type != SDL.SDL_QUIT
     while(true){
         std.debug.print("SDL Event = {any} \n", .{event.type});
-        for(0..window_size_x) |x|{
-            for(0..window_size_y) |y| {
+        for(0..@intCast(window_size_x)) |x|{
+            for(0..@intCast(window_size_y)) |y| {
                 const colors = getColors(@intCast(x),@intCast(y));
                 _ = SDL.SDL_SetRenderDrawColor(renderer,colors.red, colors.green, colors.blue, colors.alpha);
                 _ = SDL.SDL_RenderDrawPoint(renderer, @intCast(x), @intCast(y));
--- src/main.zig
+++ src/main.zig
@@ -2,15 +2,21 @@ const std = @import("std");
 const SDL = @cImport(@cInclude("SDL.h"));
 const fractals = @import("fractal_stuff.zig");
 
+const window_size_x = 1000;
+const window_size_y = 1000;
 fn colors(x:i64,y:i64) fractals.RGBA{
+    _ = x;
+    _ = y;
+
     return fractals.RGBA{
-        .red = @intCast(@rem(x,255)),
-        .green = @intCast(@rem((x-y ),255)),
-        .blue = 0,
+        .red = @intCast(@max(@rem(std.time.microTimestamp(),255),0)),
+        .green = @intCast(@max(@rem(std.time.microTimestamp(),255),0)),
+        .blue = @intCast(@max(@rem(std.time.microTimestamp(),255),0)),
         .alpha = 255
     };
 }
 
 pub fn main() !void{
-    try fractals.initFractalWindow(colors);
+    try fractals.initFractalWindow(colors,window_size_x,window_size_y);
+    try fractals.initFractalWindow(colors,window_size_x,window_size_y);
 }
